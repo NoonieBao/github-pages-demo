a5aac11d9fb9728ffe663ed6cb5911d52df71478
NoonieBao <110376555+NoonieBao@users.noreply.github.com>
2024-01-26 11:47:12 +0000

    Deploying to github-pages from @ NoonieBao/github-pages-demo@ff57ff47b3f04d3f1d1c61626bae146d0c0fc74a 🚀

--- app/vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath: './',
-  outputDir: 'dist', // dist
-  assetsDir: 'static',
-  transpileDependencies: true
-})
