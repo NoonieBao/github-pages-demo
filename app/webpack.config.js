674c91f6d594b2a9413cabfa4c5218907dbd884c
Nooniebao <java.lang.management@gmail.com>
2024-05-03 00:19:31 +0800

    retryy

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,11 +1,16 @@
 const { GitRevisionPlugin } = require('git-revision-webpack-plugin');
-const gitRevisionPlugin = new GitRevisionPlugin();
+const gitRevision = new GitRevisionPlugin();
 
 module.exports = {
   // 其他配置...
   plugins: [
     // 其他插件...
-    gitRevisionPlugin,
+    gitRevision,
     // 其他插件...
+    new webpack.DefinePlugin({
+        'process.env.VERSION': JSON.stringify(gitRevision.version()),
+        'process.env.COMMITHASH': JSON.stringify(gitRevision.commithash()),
+        'process.env.BRANCH': JSON.stringify(gitRevision.branch())
+    })
   ],
 };
