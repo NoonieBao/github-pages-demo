278960487435eee04b57e2675b358570f8f790d7
Nooniebao <java.lang.management@gmail.com>
2024-05-03 00:41:53 +0800

    000

--- app/vue.config.js
+++ app/vue.config.js
@@ -14,7 +14,19 @@ const { GitRevisionPlugin } = require('git-revision-webpack-plugin')
 const gitRevisionPlugin = new GitRevisionPlugin()
 // 使用 dayjs 格式化时间，使用前需安装依赖 npm i dayjs -S
 
+
+// module.exports = defineConfig({
+//   publicPath: './',
+//   outputDir: 'dist', // dist
+//   assetsDir: 'static',
+//   transpileDependencies: true
+// })
+
 module.exports = {
+    publicPath: './',
+  outputDir: 'dist', // dist
+  assetsDir: 'static',
+  transpileDependencies: true,
   chainWebpack: config => {
     config.plugin('define').tap(args => {
       args[0]['process.env'].COMMIT_HASH = JSON.stringify(gitRevisionPlugin.commithash())
