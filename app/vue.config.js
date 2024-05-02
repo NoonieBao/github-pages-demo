c0895a5f3d480095717a70202ce8f27fb7d87c5e
Nooniebao <java.lang.management@gmail.com>
2024-05-02 23:54:38 +0800

    5

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,23 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
-const GitRevisionPlugin = require('git-revision-webpack-plugin');
- 
-const gitRevision = new GitRevisionPlugin();
+const { GitRevisionPlugin } = require('git-revision-webpack-plugin')
 
 module.exports = defineConfig({
   publicPath: './',
   outputDir: 'dist', // dist
   assetsDir: 'static',
   transpileDependencies: true,
+  plugins: [new GitRevisionPlugin()],
 })
 
-
-plugins.push(...[
-  gitRevision,
-  new webpack.DefinePlugin({
-    'process.env.VERSION': JSON.stringify(gitRevision.version()),
-    'process.env.COMMITHASH': JSON.stringify(gitRevision.commithash()),
-    'process.env.BRANCH': JSON.stringify(gitRevision.branch()),
-  }),
-]);
-
-console.log(process.env.VERSION) 
