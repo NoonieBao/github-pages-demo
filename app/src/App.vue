a8e18d0685636d943e508532f0bcf1b72c00be08
Nooniebao <java.lang.management@gmail.com>
2024-05-03 00:53:13 +0800

    only vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <p>哈希：{{window._git.COMMIT_HASH }}</p>
-    <p>版本：{{window._git.COMMIT_HASH.substring(0,6) }}</p>
-    <p>分支：{{window._git.BRANCH }}</p>
+    <p>哈希：{{_git.COMMIT_HASH }}</p>
+    <p>版本：{{_git.COMMIT_HASH.substring(0,6) }}</p>
+    <p>分支：{{_git.BRANCH }}</p>
   </div>
 </template>
 
--- app/src/main.js
+++ app/src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
+const vm=new Vue({
   render: h => h(App),
 }).$mount('#app')
 
 console.log
-window['_git'] = Object.freeze({
+vm._git = Object.freeze({
   COMMIT_HASH: process.env.COMMIT_HASH,
   BRANCH: process.env.BRANCH,
 })
