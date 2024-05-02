4f8c99e9e25a6a109ddf874b187027b094217458
Nooniebao <java.lang.management@gmail.com>
2024-05-03 00:48:13 +0800

    done

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,8 +2,9 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <p>版本号：{{ process }}</p>
-    <p>版本号：{{ process.env.VERSION }}</p>
+    <p>哈希：{{window._git.COMMIT_HASH }}</p>
+    <p>版本：{{window._git.COMMIT_HASH.substring(0,6) }}</p>
+    <p>分支：{{window._git.BRANCH }}</p>
   </div>
 </template>
 
--- app/src/main.js
+++ app/src/main.js
@@ -7,10 +7,9 @@ new Vue({
   render: h => h(App),
 }).$mount('#app')
 
-
+console.log
 window['_git'] = Object.freeze({
   COMMIT_HASH: process.env.COMMIT_HASH,
   BRANCH: process.env.BRANCH,
-  LAST_COMMIT_DATETIME: process.env.LAST_COMMIT_DATETIME
 })
 
