e2820843790ef59c63915e82e32e5526d1b2f3ab
Nooniebao <java.lang.management@gmail.com>
2024-05-02 23:57:02 +0800

    test

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 const { GitRevisionPlugin } = require('git-revision-webpack-plugin')
 
-module.exports = defineConfig({
-  publicPath: './',
-  outputDir: 'dist', // dist
-  assetsDir: 'static',
-  transpileDependencies: true,
-  plugins: [new GitRevisionPlugin()],
-})
+module.exports = {
+  plugins: [
+    new GitRevisionPlugin({
+      branchCommand: 'rev-parse --symbolic-full-name HEAD',
+    }),
+  ],
+}
 
