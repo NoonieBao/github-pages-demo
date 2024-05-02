8ddec765cde61acabcb7642865b7be503f30e8c8
Nooniebao <java.lang.management@gmail.com>
2024-05-03 00:22:44 +0800

    q

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>版本号：{{ process }}</p>
     <p>版本号：{{ process.env.VERSION }}</p>
-    <p>提交哈希：{{ process.env.COMMITHASH }}</p>
   </div>
 </template>
 
